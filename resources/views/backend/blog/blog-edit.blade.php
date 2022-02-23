457f708416d3ddd655b7db4cac0c8ab85f5e5a85
toplancer1204 <pro.developer1204@gmail.com>
2022-02-23 10:53:53 +0200

    blog eddit

--- resources/views/backend/blog/blog-edit.blade.php
+++ resources/views/backend/blog/blog-edit.blade.php
@@ -139,7 +139,14 @@
     </script>
     <script src="https://cdn.ckeditor.com/4.14.0/standard/ckeditor.js"></script>
     <script>
-        CKEDITOR.replace('editor');
+        CKEDITOR.replace('editor',{
+        filebrowserBrowseUrl     : "{{ route('ckfinder_browser') }}",
+        filebrowserImageBrowseUrl: "{{ route('ckfinder_browser') }}?type=Images&token=123",
+        filebrowserFlashBrowseUrl: "{{ route('ckfinder_browser') }}?type=Flash&token=123",
+        filebrowserUploadUrl     : "{{ route('ckfinder_connector') }}?command=QuickUpload&type=Files",
+        filebrowserImageUploadUrl: "{{ route('ckfinder_connector') }}?command=QuickUpload&type=Images",
+        filebrowserFlashUploadUrl: "{{ route('ckfinder_connector') }}?command=QuickUpload&type=Flash",
+    });
     </script>
     <script>
         $("#coverImage").change(function () {
